5b55a6db15a7bd407ddbc84ad902121b4ffb44b4
arkanlems <estebanlemuspena@gmail.com>
2024-02-07 15:15:56 -0500

    configuration of gitignore

--- JavaIntroduction3/src/main/java/org/example/Main.java
+++ JavaIntroduction3/src/main/java/org/example/Main.java
@@ -7,40 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        Menu.mostrarMenu();
-
-        String numero = String.valueOf(12);
-
-        int [] enteros = new int[4];
-
-        List<Integer> enterosLista = new ArrayList<>();
-        List<String> estudiantesLista = new ArrayList<>();
-
-        estudiantesLista.add(("esteban"));
-        estudiantesLista.add("juan");
-        estudiantesLista.add("omar");
-        estudiantesLista.add("maria");
-        estudiantesLista.add("daniela");
-        estudiantesLista.add("daniel");
-        estudiantesLista.add("sebastian");
-
-       
-
-        /*
-
-        *key        value
-         [esteban][1]
-        * [juan][3]
-        * [Daniela][0]
-        * [Sebastian][2]
-        *
-        * */
-
-
     }
-    private int Menu(int entero){
-        entero= entero+1;
-        return 0;
-    }
+
 }
