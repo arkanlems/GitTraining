d8921a39f2fd1f7110182c77b8ac5552d5e56f1b
tduqueg <tduqueg@eafit.edu.co>
2024-02-09 11:28:22 -0500

    Declaración variables de la cuenta

---
+++ JavaIntroduction3/Tareas_Tomas_Duque/Tarea_4/Account.java
@@ -0,0 +1,10 @@
+package Dia_4.GitTraining.JavaIntroduction3.Tareas_Tomas_Duque.Tarea_4;
+
+public class Account {
+    private String name;
+    private Double amount;
+    private int account;
+    private String user;
+    private String password;
+
+}
