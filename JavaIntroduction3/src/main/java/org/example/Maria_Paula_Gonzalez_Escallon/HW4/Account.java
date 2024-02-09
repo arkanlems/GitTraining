7bdbbb3c97f321e4469ff1b13dda0aa167ff4b4e
Maria Paula <m.gonzaleze@uniandes.edu.co>
2024-02-09 16:02:19 -0500

    Add: Deposit and withdraw methods

--- JavaIntroduction3/src/main/java/org/example/Maria_Paula_Gonzalez_Escallon/HW4/Account.java
+++ JavaIntroduction3/src/main/java/org/example/Maria_Paula_Gonzalez_Escallon/HW4/Account.java
@@ -22,4 +22,18 @@ public class Account {
     public double getBalance() {
         return balance;
     }
+
+    public void withdraw(double amount) {
+        double tax = amount < 1000 ? 200 : 200 + 0.15 * amount;
+        if ((amount + tax) < balance) {
+            balance -= amount + tax;
+        }
+        else {
+            System.out.println("Oops, looks like you don't have enough money to make the transaction.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
 }
