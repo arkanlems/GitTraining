348501f8627944a1f128034ae76116702d6583ca
FelipeLadinoG <oladinog@unbosque.edu.co>
2024-02-09 16:24:47 -0500

    Client menu with withdrawals, send money and print balance

--- JavaIntroduction3/src/main/java/org/example/homeworkSession4/Controller.java
+++ JavaIntroduction3/src/main/java/org/example/homeworkSession4/Controller.java
@@ -25,7 +25,8 @@ public class Controller {
                     adminExecution();
                     break;
                 case 2:
-                    clientExecution();
+                    User user = login();
+                    clientExecution(user);
                     break;
                 default:
                     System.out.println("Enter a valid option");
@@ -59,7 +60,69 @@ public class Controller {
 
     }
 
-    public void clientExecution() {
+    public User login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter your userId: ");
+        String userId = sc.nextLine();
+        System.out.print("Please enter your password: ");
+        String password = sc.nextLine();
+
+        return userList.stream()
+                .filter(user -> user.getUserId().equals(userId) && user.getPassword().equals(password))
+                .collect(Collectors.toList()).get(0);
+
+    }
+
+    public void clientExecution(User user) {
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        do {
+            System.out.println("1. Print Balance.\n2. Send Money.\n3. Withdrawal\n4. Exit");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Account: " + user.getAccount().getNumber() +
+                            "\nBalance: " + user.getAccount().getBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter the destination Account: ");
+                    String destination = sc.nextLine();
+                    System.out.print("Enter the amount of the transaction: ");
+                    double amount = sc.nextDouble();
+
+
+                    double taxes = 100;
+
+
+                    user.getAccount().setBalance(user.getAccount().getBalance() - amount - taxes);
+
+                    Account destinationAcc = userList.stream()
+                            .map(User::getAccount)
+                            .filter(account -> account.getNumber().equals(destination))
+                            .collect(Collectors.toList()).get(0);
+                    double destinationOldBalance = destinationAcc.getBalance();
+                    destinationAcc.setBalance(amount + destinationOldBalance);
+
+                    System.out.println("Your new balance is: "+user.getAccount().getBalance());
+
+
+                    break;
+                case 3:
+                    System.out.print("Enter the amount of the transaction: ");
+                    double withdrawal = sc.nextDouble();
+                    double wTaxes = 0.0;
+                    if (withdrawal <= 1000)
+                        wTaxes = 200;
+                    else
+                        wTaxes = 200 + (withdrawal * 0.15);
+                    user.getAccount().setBalance(user.getAccount().getBalance() - withdrawal - wTaxes);
+                    System.out.println("Your new balance is: "+user.getAccount().getBalance());
+                    break;
+                default:
+                    System.out.println("Enter a valid option");
+                    break;
+            }
+        } while (option != 4);
 
 
     }
@@ -67,7 +130,7 @@ public class Controller {
     public void listUsers() {
         System.out.println("Total Users: " + userList.size());
         for (User user : userList) {
-            System.out.println("UserId: " + user.getUserId() + "\nName: " + user.getName()+
+            System.out.println("UserId: " + user.getUserId() + "\nName: " + user.getName() +
                     "\n-------------------------------------");
         }
     }
@@ -80,7 +143,7 @@ public class Controller {
         System.out.println("Total Accounts: " + accountList.size());
         for (Account account : accountList) {
             System.out.println("Number: " + account.getNumber() + "\nOpening Date: " + account.getOpeningDate() +
-                    "\nBalance: " + account.getBalance()+"\n-------------------------------------");
+                    "\nBalance: " + account.getBalance() + "\n-------------------------------------");
         }
     }
 
@@ -102,11 +165,5 @@ public class Controller {
         return new Account("ACC-" + accNumber, new Date(), 0.0, new ArrayList<>());
     }
 
-    public double addBalance(double oldBalance, double transaction) {
-        return oldBalance + transaction;
-    }
 
-    public double subtractBalance(double oldBalance, double transaction) {
-        return oldBalance - transaction;
-    }
 }
