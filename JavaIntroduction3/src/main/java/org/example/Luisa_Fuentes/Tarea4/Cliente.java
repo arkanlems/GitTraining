219f86e9ff51de8edb7980df7b600481d2c5324b
Luisa Fuentes <l.fuentesl@uniandes.edu.co>
2024-02-09 16:47:25 -0500

    Agregué la clase Cliente

---
+++ JavaIntroduction3/src/main/java/org/example/Luisa_Fuentes/Tarea4/Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private Cuenta cuenta_ahorros;
+    private String nombre_usuario;
+    private String contraseña;
+
+    public Cliente(Cuenta cuenta_ahorros, String nombre_usuario, String contraseña) {
+        this.cuenta_ahorros = cuenta_ahorros;
+        this.nombre_usuario = nombre_usuario;
+        this.contraseña = contraseña;
+    }
+
+    public Cuenta getCuenta_ahorros() {
+        return cuenta_ahorros;
+    }
+
+    public void setCuenta_ahorros(Cuenta cuenta_ahorros) {
+        this.cuenta_ahorros = cuenta_ahorros;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
