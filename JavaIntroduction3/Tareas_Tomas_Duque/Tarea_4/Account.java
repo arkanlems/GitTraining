3c78db0bc823182e41214474de6be7f493576934
tduqueg <tduqueg@eafit.edu.co>
2024-02-09 16:22:20 -0500

    Creación clase Client y Bank

--- JavaIntroduction3/Tareas_Tomas_Duque/Tarea_4/Account.java
+++ JavaIntroduction3/Tareas_Tomas_Duque/Tarea_4/Account.java
@@ -1,10 +1,8 @@
 package Dia_4.GitTraining.JavaIntroduction3.Tareas_Tomas_Duque.Tarea_4;
 
 public class Account {
-    private String name;
-    private Double amount;
-    private int account;
-    private String user;
-    private String password;
+   private String accountNumber;
+   private String openingDate;
+   private double balance;
 
 }
---
+++ JavaIntroduction3/Tareas_Tomas_Duque/Tarea_4/Client.java
@@ -0,0 +1,8 @@
+package Dia_4.GitTraining.JavaIntroduction3.Tareas_Tomas_Duque.Tarea_4;
+
+public class Client {
+    private String name;
+    private String password;
+    private Account account;
+    
+}
